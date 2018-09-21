23768ebd23915390f611c61a9be3e3a8ab527994
thiaw_mamadou <mamadou.thiaw@etu.unice.fr>
2018-09-21 15:17:13 +0200

    Architecture du projet, creation entity et session beans

---
+++ TP3ThiawNdiaye-ejb/src/java/tp3/gestionnaires/GestionnaireCompteBancaire.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp3.gestionnaires;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+@Stateless
+@LocalBean
+public class GestionnaireCompteBancaire {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
